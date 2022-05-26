5df500dfc5858af9e11e31fc6a89e9662cbeb680
haim-wix <haimha@wix.com>
2022-05-26 19:35:29 +0300

    work sun

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,53 @@
-import {createSDK} from "wix-backoffice-sdk";
+import {createSDK} from "wix-dashboard-sdk";
 
-const SDK = createSDK({origin: 'https://manage.wix.com'});
+const SDK = createSDK();
 
 function showToastButton() {
   const button = document.createElement('button');
   button.innerText = 'Show a toast';
   button.addEventListener('click', () => {
     SDK.showToast({
-      message: 'Hi! This is a message from TPA outside of Wix.'
+      message: 'Hi! This is a message from TPA outside of Wix.',
+      onCloseClick: () => {
+        const status = document.getElementById('status');
+        status!.insertAdjacentHTML('beforeend', '<div class="note">Toast close clicked!</div>');
+      }
     })
   });
   return button;
 }
 
 
-
-///////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////
-
-
-
 const title = () => `<h2 style='margin:0;padding:0;'>I'm a widget TPA!</h2>`;
 const text = () => {
   const {protocol, host} = document.location;
   const url = `${protocol}//${host}`;
-  return `<p style='margin:0;padding:0;'>You can see me at <a href="${url}" target='_blank'>${url}</a></p>`;
+  return `<p style='margin:0;padding:0;'>You can see me at <a href="${window.location}" target='_blank'>${url}</a></p>`;
 };
 
 const body = window.document.body;
 body.style.padding = '30px';
 body.style.margin = '0';
 body.style.fontFamily = 'sans-serif';
-body.insertAdjacentHTML('afterbegin', `${title()}<br />${text()}<hr />`);
+body.insertAdjacentHTML('afterbegin', `
+<style>
+@keyframes noteFadeIn {
+    0% { opacity: 0; max-height: 0; padding: 0 5px; }
+    100% { opacity: 1; max-height: 40px; padding: 5px;}
+}
+html, body {
+  overflow: hidden;
+}
+.note {
+  overflow: hidden;
+  margin-bottom: 10px;
+  border:1px solid blue;
+  animation: noteFadeIn 0.5s ease-out 0s 1 forwards;
+}
+</style>
+  ${title()}<br />
+  <div id="status"></div>
+  ${text()}
+<hr />
+`);
 body.appendChild(showToastButton());
