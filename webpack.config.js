6825e51e88665a5f062c087e8df87df9cd1ff1a6
Adam Friedmann <adamfri@wix.com>
2023-03-15 15:20:36 +0200

    change from ts to js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.ts',
+    entry: './src/index.js',
     devServer: {
         port: 55123,
         server: 'http',
